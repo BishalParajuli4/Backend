57a922050dabd245882a7f282d63256f961e26b3
BishalParajuli4 <124025748+BishalParajuli4@users.noreply.github.com>
2025-04-30 13:06:46 +0545

    main

--- expressjs/src/main.ts
+++ expressjs/src/main.ts
@@ -1,3 +1,4 @@
+import { error } from "console";
 import express, {Request, Response} from "express";
 const app = express();
 
@@ -11,8 +12,19 @@ interface Product {
 }
 
 const products: Product[] = [];
+app.use(express.json());
+
 app.post("/products",( req: Request, res: Response)=>{
     const{name, price, description} = req.body
+
+    if(!name || !price || !description){
+        res.status(400).json({error: "Please provide appropriate data's"});
+    };
+
+    // if(!price){
+    //     res.status(400).json({error: "Please Enter the appropriate price"})
+    // }
+
     const newProduct = {
         id: products.length + 1,
         name: name, 
@@ -23,6 +35,10 @@ app.post("/products",( req: Request, res: Response)=>{
     res.status(200).json(newProduct);
     
 });
+
+app.get ('/products',(req:Request , res:Response)=>{
+    res.json(products);
+})
 
 app.get('/',(req: Request, res: Response)=>{
     res.send("Hello World");
